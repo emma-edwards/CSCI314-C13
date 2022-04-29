15eba11c1feb5ae2a6d616933e2a910e211f0982
emma-edwards <eee946@uowmail.edu.au>
2022-04-29 16:29:12 +1000

    working on page structure

--- my-react-app/src/Pages/Home.js
+++ my-react-app/src/Pages/Home.js
@@ -1,5 +1,22 @@
+import { Outlet, Link as RouterLink } from "react-router-dom";
+
 const Home = () => {
-    return <h1>Home</h1>;
+    return (
+        <>
+            <h1>Home</h1>;
+
+            <RouterLink to="/SignIn">Customer Sign In</RouterLink>
+            <br />
+            <RouterLink to="/SignUp">Customer Sign Up</RouterLink>
+            <br />
+            <RouterLink to="/ProfessionalReg">Professional Registration</RouterLink>
+            <br />
+
+            <Outlet />
+
+        </>
+    )
+
 };
 
 export default Home;
--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 //import App from './App';
 import Layout from './Pages/Layout';
+import Nav from './Pages/Nav';
 import Home from './Pages/Home';
 import SignIn from './Pages/SignIn';
 import Subscriptions from './Pages/Subscriptions';
@@ -14,13 +15,15 @@ import ProfessionalReg from "./Pages/ProfessinalReg";
 import VehicleReg from "./Pages/VehicleReg";
 import SubmitClaim from "./Pages/SubmitClaim";
 import SignUp from "./Pages/SignUp";
+import MemberHome from './Pages/MemberHome';
 
 
 export default function App() {
     return (
-        <BrowserRouter>
+        /*<BrowserRouter>
             <Routes>
-                <Route path="/" element={<Layout />}>
+                {/!* <Route path="/" element={<Layout />}> *!/}
+                <Route path="/" element={<Nav />}>
                     <Route index element={<Home />} />
                     <Route path="SignIn" element={<SignIn />} />
                     <Route path="Subscriptions" element={<Subscriptions />} />
@@ -30,10 +33,34 @@ export default function App() {
                     <Route path="VehicleReg" element={<VehicleReg />} />
                     <Route path="SubmitClaim" element={<SubmitClaim />} />
                     <Route path="SignUp" element={<SignUp />} />
+                    <Route path="MemberHome" element={<MemberHome />} />
 
 
 
                 </Route>
+            </Routes>
+
+        </BrowserRouter>*/
+
+        <BrowserRouter>
+            <Routes>
+                {/* <Route path="/" element={<Layout />}> */}
+                <Route path="/" element={<Home />}>
+                    <Route path="SignIn" element={<SignIn />} />
+                    <Route path="SignUp" element={<SignUp />} />
+                    <Route path="ProfessionalReg" element={<ProfessionalReg />} />
+                    <Route path="MemberHome" element={<Nav />}>
+                        <Route index element={<MemberHome />} />
+                        <Route path="Subscriptions" element={<Subscriptions />} />
+                        <Route path="Pricing" element={<Pricing />} />
+                        <Route path="EditMembership" element={<EditMembership />} />
+                        <Route path="VehicleReg" element={<VehicleReg />} />
+                        <Route path="SubmitClaim" element={<SubmitClaim />} />
+                        <Route path="MemberHome" element={<MemberHome />} />
+                    </Route>
+                </Route>
+
+
             </Routes>
 
         </BrowserRouter>
