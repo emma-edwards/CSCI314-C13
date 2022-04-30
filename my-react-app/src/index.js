42f88dc3773635990b985150a550f2094778e30d
emma-edwards <eee946@uowmail.edu.au>
2022-04-30 21:10:20 +1000

    working on page structure

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -20,28 +20,6 @@ import MemberHome from './Pages/MemberHome';
 
 export default function App() {
     return (
-        /*<BrowserRouter>
-            <Routes>
-                {/!* <Route path="/" element={<Layout />}> *!/}
-                <Route path="/" element={<Nav />}>
-                    <Route index element={<Home />} />
-                    <Route path="SignIn" element={<SignIn />} />
-                    <Route path="Subscriptions" element={<Subscriptions />} />
-                    <Route path="Pricing" element={<Pricing />} />
-                    <Route path="EditMembership" element={<EditMembership />} />
-                    <Route path="ProfessionalReg" element={<ProfessionalReg />} />
-                    <Route path="VehicleReg" element={<VehicleReg />} />
-                    <Route path="SubmitClaim" element={<SubmitClaim />} />
-                    <Route path="SignUp" element={<SignUp />} />
-                    <Route path="MemberHome" element={<MemberHome />} />
-
-
-
-                </Route>
-            </Routes>
-
-        </BrowserRouter>*/
-
         <BrowserRouter>
             <Routes>
                 {/* <Route path="/" element={<Layout />}> */}
