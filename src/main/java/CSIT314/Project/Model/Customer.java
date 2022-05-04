0b542b2b32810141bc62ecf86e01361e79ad7ba4
Angus Dumaresq <angusdq@gmail.com>
2022-05-04 16:39:20 +1000

    Commit Message

--- src/main/java/CSIT314/Project/Model/Customer.java
+++ src/main/java/CSIT314/Project/Model/Customer.java
@@ -1,10 +1,15 @@
 package CSIT314.Project.Model;
 
+import lombok.Builder;
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Data
+@Builder
 @Entity
 public class Customer {
     @Id
