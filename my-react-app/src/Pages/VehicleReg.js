3cbfeac13ec33cee990625abcdd92c1c0b908fbc
Angus Dumaresq <angusdq@gmail.com>
2022-05-05 12:21:57 +1000

    commit on master

--- my-react-app/src/Pages/VehicleReg.js
+++ my-react-app/src/Pages/VehicleReg.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useEffect, useState } from 'react';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
@@ -7,6 +8,45 @@ import Checkbox from '@mui/material/Checkbox';
 import Button from "@mui/material/Button";
 
 export default function VehicleReg() {
+    const[plate,setPlate]=useState('')
+    const[vehiceMake,setVehicleMake]=useState('')
+    const[price,setPrice]=useState('')
+    const[insuranceProvider,setInsuranceProvider]=useState('')
+
+    const checkTextInput = (e) => {
+        if (!plate.trim()) {
+            alert('Please Enter Plate Number'); 
+            window.location.href = '/VehicleReg'
+        }else if (!vehiceMake.trim()) {
+            alert('Please Enter Vehicle Make');
+            window.location.href = '/VehicleReg'
+        }else if (!price.trim()) {
+            alert('Please Enter Price');
+            window.location.href = '/VehicleReg'
+        }else if (!insuranceProvider.trim()) {
+            alert('Please Enter Insurance Provider');
+            window.location.href = '/VehicleReg'
+        }else{
+            handleClick(e);
+        }
+    }
+
+    const handleClick=(e)=>{
+        e.preventDefault()
+        const vehicle={plate, vehiceMake, price, insuranceProvider}
+        console.log(vehicle)
+        fetch("http://localhost:8080/vehicle/addVehicle",{
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify(vehicle)
+        }).then(()=>{
+            console.log("Vehicle added")
+            window.location.href = '/MemberHome'
+        })
+    }
+
+
+
     return (
         <React.Fragment>
             <Typography variant="h6" gutterBottom>
@@ -21,6 +61,8 @@ export default function VehicleReg() {
                         label="Plate Number"
                         fullWidth
                         variant="standard"
+                        value={plate}
+                        onChange={(e)=>setPlate(e.target.value)}
                     />
                 </Grid>
                 <Grid item xs={12} sm={6}>
@@ -31,6 +73,8 @@ export default function VehicleReg() {
                         label="Vehicle Make"
                         fullWidth
                         variant="standard"
+                        value={vehiceMake}
+                        onChange={(e)=>setVehicleMake(e.target.value)}
                     />
                 </Grid>
                 <Grid item xs={12}>
@@ -41,6 +85,8 @@ export default function VehicleReg() {
                         label="Price"
                         fullWidth
                         variant="standard"
+                        value={price}
+                        onChange={(e)=>setPrice(e.target.value)}
                     />
                 </Grid>
                 <Grid item xs={12}>
@@ -51,9 +97,12 @@ export default function VehicleReg() {
                         label="Insurance Provider"
                         fullWidth
                         variant="standard"
+                        value={insuranceProvider}
+                        onChange={(e)=>setInsuranceProvider(e.target.value)}
                     />
                 </Grid>
-                <Button href="#" variant="outlined" sx={{ my: 1, mx: 1.5 }}>
+                <Button href="#" variant="outlined" sx={{ my: 1, mx: 1.5 }}
+                      onClick={checkTextInput}>
                     Submit Registration
                 </Button>
                 <Button href="#" variant="outlined" sx={{ my: 1, mx: 1.5 }}>
