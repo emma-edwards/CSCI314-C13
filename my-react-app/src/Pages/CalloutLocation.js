0235258c73168195ab6eeaa584307132982aeda9
Joshua Durston <102574362+JoshuaDurston@users.noreply.github.com>
2022-05-11 15:37:51 +1000

    added map to calloutLocation page

--- my-react-app/src/Pages/CalloutLocation.js
+++ my-react-app/src/Pages/CalloutLocation.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Hook,{useState, useRef, useEffect} from 'react';
+ import Hook,{useState, useRef, useEffect} from 'react';
 //import {Wrapper, Status} from "@googlemaps/react-wrapper";
 import {Map, GoogleApiWrapper, Marker} from 'google-maps-react';
 //Map CSS
@@ -9,17 +9,19 @@ const MapStyles = {
 };
 
 
+
 export class  CalloutLocation extends React.Component{
     //CONSTRUCTOR
     constructor(props) {
         super(props);
         this.state = {
             stores: [{lat: -34.405, lng: 150.878},
-                    {latitude: -34.405, longitude: 150.878},
-                    {latitude: -34.424, longitude: 150.882},
-                    {latitude: -34.421, longitude: 150.909}]
+                {latitude: -34.405, longitude: 150.878},
+                {latitude: -34.424, longitude: 150.882},
+                {latitude: -34.421, longitude: 150.909}]
         }
     }
+
     //FUNCTIONS
     displayMarkers = () => {
         return this.state.stores.map((store, index) => {
@@ -27,7 +29,8 @@ export class  CalloutLocation extends React.Component{
                 lat: store.latitude,
                 lng: store.longitude
             }}
-                           onClick={() => alert("You clicked a marker")}/>
+                           onClick={() => alert("You clicked a marker")}
+            />
         })
     }
 
@@ -48,7 +51,7 @@ export class  CalloutLocation extends React.Component{
             </h1>
         );
     }
-};
+}
 
 export default GoogleApiWrapper(
     {
--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -15,15 +15,12 @@ import ProfessionalReg from "./Pages/ProfessinalReg";
 import VehicleReg from "./Pages/VehicleReg";
 import SubmitClaim from "./Pages/SubmitClaim";
 import SignUp from "./Pages/SignUp";
-<<<<<<<<< Temporary merge branch 1
 import CalloutLocation from "./Pages/CalloutLocation";
-=========
 import MemberHome from './Pages/MemberHome';
 import ProfessionalHome from './Pages/ProfessionalHome';
 import AvJobs from './Pages/AvJobs';
 import CurrentJobs from './Pages/CurrentJobs';
 import PNav from './Pages/PNav';
->>>>>>>>> Temporary merge branch 2
 
 
 export default function App() {
@@ -44,7 +41,6 @@ export default function App() {
                     <Route path="EditMembership" element={<EditMembership />} />
                     <Route path="VehicleReg" element={<VehicleReg />} />
                     <Route path="SubmitClaim" element={<SubmitClaim />} />
-<<<<<<<<< Temporary merge branch 1
                     <Route path="SignUp" element={<SignUp />} />
                     <Route path="CalloutLocation" element={<CalloutLocation />} />
 
