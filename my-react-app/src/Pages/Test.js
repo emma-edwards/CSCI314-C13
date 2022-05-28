24aa6ef3289492d2a4b08501e7738ea4eff31f0b
Angus Dumaresq <angusdq@gmail.com>
2022-05-28 10:10:29 +1000

    Load data page added

--- my-react-app/src/Pages/Test.js
+++ my-react-app/src/Pages/Test.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { useEffect, useState } from 'react';
-import Button from '@mui/material/Button';
 
 export default function useTest(){
     const[customers,setCustomer]=useState([])
+    const[requests,setRequest]=useState([])
+    const[professionals,setProfessional]=useState([])
 
     const handleClick=(e)=>{
         //Loads 20 Customers 
@@ -23,10 +24,52 @@ export default function useTest(){
                 headers:{"Content-Type":"application/json"},
                 body:JSON.stringify(customer)
             }).then(()=>{
-                console.log("Customer added")
+                console.log(customer)
+            })
+        }
+
+        //Loads 20 Requests 
+        for (i = 0; i < 20; i++) {
+            const sNumber = "firstName" + i;
+            const streetName = "lastName" + i;
+            const suburb = "password" + i;
+            const report = "email" + i + "@email.com";
+
+
+            e.preventDefault()
+            const request={sNumber, streetName, suburb, report}
+            console.log(request)
+            fetch("http://localhost:8080/request/addRequest",{
+                method:"POST",
+                headers:{"Content-Type":"application/json"},
+                body:JSON.stringify(request)
+            }).then(()=>{
+                console.log(request)
+            })
+        }
+
+        //Loads 100 professionals
+        for (i = 0; i < 100; i++) {
+            const firstName = "pFirstName" + i;
+            const lastName = "pLastName" + i;
+            const dob = "01/01/20" + i;
+            const email = "pemail" + i + "@professional.com";
+            const phone = "12345" + i;
+            const yearsWorked = i;
+
+            e.preventDefault()
+            const professional={firstName, lastName, dob, email, phone, yearsWorked}
+            console.log(professional)
+            fetch("http://localhost:8080/professional/addProfessional",{
+                method:"POST",
+                headers:{"Content-Type":"application/json"},
+                body:JSON.stringify(professional)
+            }).then(()=>{
+                console.log(professional)
             })
         }
     }
+
 
     useEffect(()=>{
         fetch("http://localhost:8080/customer/getAll")
@@ -37,9 +80,29 @@ export default function useTest(){
       )
       },[])
 
+      useEffect(()=>{
+        fetch("http://localhost:8080/request/getAll")
+        .then(res=>res.json())
+        .then((result)=>{
+          setRequest(result);
+        }
+      )
+      },[])
+
+      useEffect(()=>{
+        fetch("http://localhost:8080/professional/getAll")
+        .then(res=>res.json())
+        .then((result)=>{
+          setProfessional(result);
+        }
+      )
+      },[])
+
     return(
         <div>
-            <Button onClick={handleClick}>Load Test Data</Button>     
+            <button onClick={handleClick}>Load Test Data</button>     
+            <br></br>
+            <p>Refresh the page after loading the data</p>
             <br></br>
             <p>This data shows 20 customers</p>   
             <table className="customerTable">
@@ -60,7 +123,48 @@ export default function useTest(){
                         )
                     }
                 </tbody>
-            </table>  
+            </table> 
+
+
+            <br></br>
+            <p>This data shows 20 requests</p>  
+            <table className="requestTable">
+                <tbody> 
+                    {
+                        requests.map(
+                            request=>
+                                <tr key={request.id}>    
+                                        <td>{request.id}</td>
+                                        <td>{request.sNumber}</td>
+                                        <td>{request.streetName}</td>
+                                        <td>{request.suburb}</td>
+                                        <td>{request.report}</td>                                    
+                                </tr>
+                        )
+                    }
+                </tbody>
+            </table>     
+
+            <br></br>
+            <p>This data shows 100 professionals</p>  
+            <table className="professionalTable">
+                <tbody> 
+                    {
+                        professionals.map(
+                            professional=>
+                                <tr key={professional.id}>    
+                                        <td>{professional.id}</td>
+                                        <td>{professional.firstName}</td>     
+                                        <td>{professional.lastName}</td>
+                                        <td>{professional.dob}</td>
+                                        <td>{professional.email}</td>
+                                        <td>{professional.phone}</td>
+                                        <td>{professional.yearsWorked}</td>                           
+                                </tr>
+                        )
+                    }
+                </tbody>
+            </table>   
         </div>
     );
 }
