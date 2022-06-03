5d4c583293b1ef93d0252ae9742b86d53bf49ea0
Angus Dumaresq <angusdq@gmail.com>
2022-06-03 15:24:06 +1000

    Updates

--- src/main/java/CSIT314/Project/Model/Request.java
+++ src/main/java/CSIT314/Project/Model/Request.java
@@ -14,15 +14,17 @@ public class Request {
     private String streetName;
     private String suburb;
     private String report;
+    private String total;
 
     public Request(){}
 
-    public Request(Long id, String sNumber, String streetName, String suburb, String report) {
+    public Request(Long id, String sNumber, String streetName, String suburb, String report, String total) {
         this.id = id;
         this.sNumber = sNumber;
         this.streetName = streetName;
         this.suburb = suburb;
         this.report = report;
+        this.total = total;
     }
 
     public Long getId() {
@@ -65,10 +67,19 @@ public class Request {
         this.report = report;
     }
 
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
     public void setRequest(Request request){
         setsNumber(request.sNumber);
         setSuburb(request.suburb);
         setStreetName(request.streetName);
         setReport(request.report);
+        setTotal(request.total);
     }
 }
